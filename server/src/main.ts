8657226865acd3f50a97d26ec2ed4313c0fef852
markflerko <markflerko@gmail.com>
2022-05-09 21:47:07 +0400

    feat: init client

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   try {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
+
+    app.enableCors();
+
     app.useGlobalPipes(
       new ValidationPipe({
         transform: true,
